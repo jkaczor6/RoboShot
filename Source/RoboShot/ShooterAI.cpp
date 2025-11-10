63e30b3f24e3d77f61ea5c8974082eeb75e69367
Jakub Kaczor <j.kaczor06@gmail.com>
2025-11-10 15:50:55 +0100

    Making enemy focus towards player, started coding Enemy AI

---
+++ Source/RoboShot/ShooterAI.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAI.h"
+
+void AShooterAI::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (PlayerPawn)
+	{
+		SetFocus(PlayerPawn);
+	}
+}
---
+++ Source/RoboShot/ShooterAI.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "ShooterAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOSHOT_API AShooterAI : public AAIController
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	APawn* PlayerPawn;
+};
